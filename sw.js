16d116092c1b67033c78a4d76eba1043661a7555
BinooProg <binooprog@gmail.com>
2026-06-21 13:36:51 +0300

    Big style changes

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy for offline support
    ============================================ */
 
-const CACHE_VERSION = 'quickpay-v6';
+const CACHE_VERSION = 'quickpay-v11';
 
 const STATIC_ASSETS = [
     './',
